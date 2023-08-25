f29e5f92fc87a6cb7a7c03c73a1198a1c0a8786f
Oleg Rubel <rubelo@mcmaster.ca>
2023-08-25 09:48:14 -0400

    Clean up

--- tests/test_fontsize.py
+++
@@ -1,8 +0,0 @@
-import pydef_core.cell as pc
-
-directory = './tests/test_files/'
-gga = pc.Cell(directory + 'Functionals/CdIn2S4 - GGA/OUTCAR', directory + 'Functionals/CdIn2S4 - GGA/DOSCAR')
-dpp = pc.DosPlotParameters(gga)
-dpp.text_size = 60
-
-gga.plot_dos(None, dpp)
